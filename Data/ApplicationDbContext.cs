485a7d9334ac020905eec58ecf840156f8ad625c
syedajabeenali <147488179+syedajabeenali@users.noreply.github.com>
2025-03-07 15:03:23 +0500

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EvidenceVault.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    }
+}
